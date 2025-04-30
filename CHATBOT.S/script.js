9e9eb01a88f9d30d0b76bed684c514d9fa8c731e
Piyush Suryawanshi <piyushsuryawanshi11@gmail.com>
2025-05-01 02:13:00 +0530

    Add files via upload

--- CHATBOT.S/script.js
+++ CHATBOT.S/script.js
@@ -1,8 +1,12 @@
 const typingForm = document.querySelector(".typing-form");
 const chatlist = document.querySelector(".chat-list");
+const suggestions = document.querySelectorAll(".suggestion-list .suggestion");
 const toggleThemeButton = document.querySelector("#toggle-theme-button");
+const deleteChatButton = document.querySelector("#delete-chat-button");
 
 let userMessage = null;
+let isResponseGenerating = false;
+
 
 //API configuration
 const API_KEY = "AIzaSyAbDnA51nme2d2f5QNJPBj-9UTRIDSLeww";
@@ -19,6 +23,11 @@ const loadLocalstorageData = () =>  {
 
     // restore saved chats
     chatlist.innerHTML = savedChats || "";
+
+    document.body.classList.toggle("hide-header",savedChats);
+    chatlist.scrollTo(0, chatlist.scrollHeight); // scrool to the botttom
+
+
 }
 
 loadLocalstorageData();
@@ -44,6 +53,8 @@ const showTypingEffect = (text, textElement) => {
 // if all words are displayed
         if(currentWordIndex === words.length) {
             clearInterval(typingInterval);
+            isResponseGenerating = false;
+            incomingMessageDiv.querySelector(".icon").classList.remove("hide");
             localStorage.setItem("savedChats",chatlist.innerHTML ); //save chats to local storage
         }
     }, 75);
@@ -65,6 +76,7 @@ const generateAPIResponse = async (incomingMessageDiv) => {
         });
 
        const data =  await response.json();
+       if(!response.ok) throw new Error(data.error.message);
 
        //Get the API response text
        const apiResponse = data?.candidates[0].content.parts[0].text;
@@ -72,7 +84,9 @@ const generateAPIResponse = async (incomingMessageDiv) => {
        showTypingEffect(apiResponse, textElement);
 
     } catch (error) {
-        console.log(error);
+        isResponseGenerating = false;
+        textElement.innerText = error.message;
+        textElement.classList.add("error");
     }finally {
     incomingMessageDiv.classList.remove("loading");
     }
@@ -113,29 +127,51 @@ const copyMessage = (copyIcon)  => {
 
 // Handle sending outgoing chat messages
 const handleOutgoingChat = () => {
-    userMessage = typingForm.querySelector(".typing-input").value.trim(); // Get user input
-    if (!userMessage) return; // Exit if there is no message
+    userMessage = typingForm.querySelector(".typing-input").value.trim() || userMessage; // Get user input
+    if (!userMessage || isResponseGenerating) return; // Exit if there is no message
 
-    const html = `
-        <div class="message-content">
+    isResponseGenerating = true;
+
+    const html = `<div class="message-content">
             <img src="image/piyush.user.jpg" alt="User's Image" class="avatar" />
             <p class="text">${userMessage}</p>
         </div>`;
 
     const outgoingMessageDiv = createMessageElement(html, "outgoing");  
+    outgoingMessageDiv.querySelector(".text").innerText = userMessage;
     chatlist.appendChild(outgoingMessageDiv); // Append the message to chatlist
 
   
     // Show loading animation after the outgoing message
     typingForm.reset();
+    chatlist.scrollTo(0, chatlist.scrollHeight);//scroll to the bottom
+    document.body.classList.add("hide-header");// hide the header once chat start
     setTimeout(showLoadingAnimation, 500); // Show loading animation after 500ms
 }
+
+//set usermessage and handle outgoing chat whan a suggestion is clicked
+suggestions.forEach(suggestion => {
+    suggestion.addEventListener("click", () => {
+        userMessage = suggestion.querySelector(".text").innerText
+        handleOutgoingChat();
+    });
+});
+
 
 //toggle between light and dark themes
 toggleThemeButton.addEventListener("click", () => {
     const isLightMode = document.body.classList.toggle("light_mode");
     localStorage.setItem("themeColor", isLightMode ? "light_mode" : "dark_mode");
     toggleThemeButton.innerText = isLightMode ? "dark_mode" : "light_mode";
+});
+
+
+// delete all chat from the page
+deleteChatButton.addEventListener("click",() => {
+    if(confirm("Are you sure bro... you want to delete all message?")) {
+        localStorage.removeItem("savedChats");
+        loadLocalstorageData();
+    }
 });
 
 // Prevent default form submission and handle outgoing chat
